97734697f01f26e66655cb1fdb095c6f2537cd9e
Nanto <nanto.rokhani@kiranatama.com>
2011-03-22 13:22:50 +0700

    First Commited Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microtest::Application.config.secret_token = 'd0802b70e550c01f2dfd45f3969b8e7a16ba36e88970be545dab5ddc9f12b95c78674f1a73809766961af7b285a9a9fec3a8a8f311650d6e0c1a4b11a8675001'
